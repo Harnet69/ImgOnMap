3f96782fd9bf231d4fb22e76d3ab337c0e0c756e
Harnet69 <harnet69@gmail.com>
2020-07-25 15:01:45 +0200

    Permission asking works correctly

--- app/src/main/java/com/harnet/mapapp/controller/PermissionController.java
+++ app/src/main/java/com/harnet/mapapp/controller/PermissionController.java
@@ -2,15 +2,13 @@ package com.harnet.mapapp.controller;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import static androidx.core.app.ActivityCompat.requestPermissions;
 
 public class PermissionController {
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
@@ -23,77 +21,11 @@ public class PermissionController {
         this.mapActivity = mapActivity;
     }
 
-    // check if permission have granted already, if didn't - asks for it
+    // check if permission have granted already
     public void checkLocationPermission() {
         Log.i("MapAppCheck", "checkLocationPermission: ");
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(mapActivity,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                alertDialog();
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(mapActivity,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION);
-            }
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(mapActivity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
         }
     }
-
-    private void alertDialog(){
-        new AlertDialog.Builder(context)
-                .setTitle("Location permission")
-                .setMessage("App need your permission")
-                .setPositiveButton("Give", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //Prompt the user once explanation has been shown
-                        ActivityCompat.requestPermissions(mapActivity,
-                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                MY_PERMISSIONS_REQUEST_LOCATION);
-                    }
-                })
-                .create()
-                .show();
-    }
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay!
-                    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-                            != PackageManager.PERMISSION_GRANTED
-                            && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//        System.out.println("DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");
-//                    Log.i("MapAppCheck", "onRequestPermissionsResult: WORK");
-                        return;
-                    }
-                    //TODO update user location(you can define time or distance for saving battery life)
-                    // refresh activity after permission granted
-                    mapActivity.finish();
-//                    startActivity(getIntent());
-//                    // location-related task you need to do.
-//                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//                        //Request location updates:
-//                        if(provider != null){
-//                            locationManager.requestLocationUpdates(provider, 10000, 0, locationListener);
-//                        }
-//                    }
-//                } else {
-//                    // permission denied, boo! Disable a functionality that depends on this permission.
-//                    Intent mainActivityIntent = new Intent(this, MainActivity.class);
-//                    startActivity(mainActivityIntent);
-                }
-                return;
-            }
-        }
-    }
-
 }
--- app/src/main/java/com/harnet/mapapp/view/MapsFragment.java
+++ app/src/main/java/com/harnet/mapapp/view/MapsFragment.java
@@ -15,15 +15,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.harnet.mapapp.R;
 import com.harnet.mapapp.controller.MapController;
@@ -36,8 +33,6 @@ public class MapsFragment extends Fragment  {
 
     private MapController mapController;
     private PermissionController permissionController;
-
-    private Marker marker = null;
     private LocationManager locationManager;
     private LocationListener locationListener;
     private String provider;
@@ -104,14 +99,6 @@ public class MapsFragment extends Fragment  {
             mapFragment.getMapAsync(callback);
         }
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.i("MapAppCheck", "onRequestPermissionsResult: ");
-//        permissionController.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
 
     public interface OnMessageSendListener {
         public void onMessageSend(String message);
